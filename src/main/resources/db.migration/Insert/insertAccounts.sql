4b5b87f35a59ab460c5dea05ea9c47a1b0c90d85
Sun-2302 <hei.dera.932@gmail.com>
2023-12-04 23:56:51 +0300

    feat: add insert for table Account

---
+++ src/main/resources/db.migration/Insert/insertAccounts.sql
@@ -0,0 +1,4 @@
+insert into account (IDCURRENCY)
+values ('c68b5444-92e2-11ee-b9d1-0242ac120002'),
+       ('c68b5444-92e2-11ee-b9d1-0242ac120002'),
+       ('c68b573c-92e2-11ee-b9d1-0242ac120002');
