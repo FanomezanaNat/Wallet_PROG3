350d5d580fa01006c117b8fb2bfa4f8397aa993a
Sun-2302 <hei.dera.932@gmail.com>
2023-12-04 23:56:29 +0300

    feat: add insert for table Currency

---
+++ src/main/resources/db.migration/Insert/insertCurrencies.sql
@@ -0,0 +1,4 @@
+insert into currency (id, type)
+values ('c68b5444-92e2-11ee-b9d1-0242ac120002','Malagasy Ariary'),
+       ('c68b573c-92e2-11ee-b9d1-0242ac120002','Euro'),
+       ('c68b5b10-92e2-11ee-b9d1-0242ac120002','Bitcoin');
