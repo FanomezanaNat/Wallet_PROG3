5e7842964fc3c4b74a24511c8dcd35a6c602ace0
Sun-2302 <hei.dera.932@gmail.com>
2023-12-04 23:57:13 +0300

    feat: add insert for table Transaction

---
+++ src/main/resources/db.migration/Insert/insertTransactions.sql
@@ -0,0 +1,4 @@
+insert into transaction (id, type, transactiondate, amount, idaccount)
+values ('1f904e5a-92e3-11ee-b9d1-0242ac120002','food and drink','2023-12-02 12:17',10000,1),
+       ('1f905152-92e3-11ee-b9d1-0242ac120002','gifts','2023-12-04 15:24',20,2),
+       ('1f9052ba-92e3-11ee-b9d1-0242ac120002','online shopping','2023-11-30 17:20',25000,1);
