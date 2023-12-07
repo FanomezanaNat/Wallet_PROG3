0ccbf4d046cab4bcca5b0d6ee1eeaa240d260453
FanomezanaNat <hei.fanomezana.2@gmail.com>
2023-12-07 10:46:12 +0300

    feat: add new columns in tables

--- src/main/resources/db.migration/Wallet_app_database_structure.sql
+++ src/main/resources/db.migration/Wallet_app_database_structure.sql
@@ -1,25 +1,32 @@
 create table IF NOt EXISTS currency
 (
     id   uuid primary key not null,
-    type varchar(50)      not null
+    name varchar(50)      not null,
+    code varchar(25)      not null
 );
 
 
 
 create table IF NOT EXISTS account
 (
-    id         serial primary key not null,
-    idCurrency uuid references currency (id)
+
+    id         uuid primary key not null,
+    name       varchar(150),
+    balance    double precision,
+    updateDate timestamp default current_timestamp,
+    type       varchar(150)     not null,
+    Currency   uuid references currency (id)
 );
 
 
 create table IF NOT EXISTS transaction
 (
     id              uuid primary key                    not null,
+    label           varchar(100)                        not null,
     type            varchar(150)                        not null,
     transactionDate timestamp default current_timestamp not null,
     amount          integer                             not null,
-    idAccount       integer references account (id)
+    Account         uuid references account (id)
 );
 
 
