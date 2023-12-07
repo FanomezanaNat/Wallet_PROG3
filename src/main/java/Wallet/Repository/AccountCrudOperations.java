bc4e58c2dfe14a15c7877be5c70f00bc26ea20a1
Sun-2302 <hei.dera.932@gmail.com>
2023-12-07 22:05:23 +0300

    feat: add new attributes to AccountCrudOperations

--- src/main/java/Wallet/Repository/AccountCrudOperations.java
+++ src/main/java/Wallet/Repository/AccountCrudOperations.java
@@ -1,6 +1,7 @@
 package Wallet.Repository;
 
 import Wallet.Entity.Account;
+import Wallet.Entity.Transaction;
 import lombok.AllArgsConstructor;
 
 import java.sql.Connection;
@@ -18,13 +19,33 @@ public class AccountCrudOperations implements CrudOperations<Account>{
     @Override
     public List<Account> findAll() {
         List<Account> accountList = new ArrayList<>();
-        String sql = "SELECT * from account";
+        List<Transaction> transactionList = new ArrayList<>();
+
+        String sql = "SELECT account.id idAccount, account.name AccountName, balance, updatedate, account.type AccountType," +
+                "t.id idTransaction, label, t.type TransactionType, transactiondate, amount\n" +
+                "from account\n" +
+                "inner join transaction t on account.id = t.account";
 
         try (PreparedStatement statement = connection.prepareStatement(sql)){
             ResultSet resultSet = statement.executeQuery();
             while (resultSet.next()) {
+
+                transactionList.add(new Transaction(
+                        (UUID) resultSet.getObject("idTransaction"),
+                        resultSet.getString("label"),
+                        resultSet.getString("TransactionType"),
+                        resultSet.getTimestamp("transactiondate"),
+                        resultSet.getDouble("amount"),
+                        (UUID) resultSet.getObject("idAccount")
+                ));
+
                 accountList.add(new Account(
-                        resultSet.getInt("id"),
+                        (UUID) resultSet.getObject("idAccount"),
+                        resultSet.getString("accountName"),
+                        resultSet.getDouble("balance"),
+                        resultSet.getTimestamp("updatedate"),
+                        transactionList,
+                        resultSet.getString("AccountType"),
                         (UUID) resultSet.getObject("idCurrency")
                 ));
             }
@@ -49,11 +70,15 @@ public class AccountCrudOperations implements CrudOperations<Account>{
 
     @Override
     public Account save(Account toSave) {
-        String sql = "INSERT INTO account(id, idcurrency) values(?,?)";
+        String sql = "INSERT INTO account(id, name, balance, updateDate, type, currency) values(?,?,?,?,?,?)";
 
         try (PreparedStatement statement = connection.prepareStatement(sql)){
-            statement.setInt(1, toSave.getId());
-            statement.setObject(2, toSave.getIdCurrency());
+            statement.setObject(1, toSave.getId());
+            statement.setString(2, toSave.getName());
+            statement.setDouble(3, toSave.getBalance());
+            statement.setTimestamp(4, toSave.getUpdateDate());
+            statement.setString(5, toSave.getType());
+            statement.setObject(6, toSave.getCurrency());
 
             int rowAffected = statement.executeUpdate();
             if (rowAffected > 0) {
