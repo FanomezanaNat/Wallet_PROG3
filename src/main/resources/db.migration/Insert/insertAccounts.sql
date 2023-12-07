34c3aff1da75dac59f77652be4115aec6fd0a730
Sun-2302 <hei.dera.932@gmail.com>
2023-12-07 11:08:17 +0300

    feat: add new columns to the mock data

--- src/main/resources/db.migration/Insert/insertAccounts.sql
+++ src/main/resources/db.migration/Insert/insertAccounts.sql
@@ -1,4 +1,4 @@
-insert into account (IDCURRENCY)
-values ('c68b5444-92e2-11ee-b9d1-0242ac120002'),
-       ('c68b5444-92e2-11ee-b9d1-0242ac120002'),
-       ('c68b573c-92e2-11ee-b9d1-0242ac120002');
+insert into account (id, name, balance, type, currency)
+values ('98ac0770-92e3-11ee-b9d1-0242ac120002','',100000,'Cash','98abfe06-92e3-11ee-b9d1-0242ac120002'),
+       ('98ac0928-92e3-11ee-b9d1-0242ac120002','Current Account',230000,'Mobile Money','98abfe06-92e3-11ee-b9d1-0242ac120002'),
+       ('98ac0b4e-92e3-11ee-b9d1-0242ac120002','Saving Account',50,'Bank','98ac0590-92e3-11ee-b9d1-0242ac120002');
--- src/main/resources/db.migration/Insert/insertTransactions.sql
+++ src/main/resources/db.migration/Insert/insertTransactions.sql
@@ -1,4 +1,4 @@
-insert into transaction (id, type, transactiondate, amount, idaccount)
-values ('1f904e5a-92e3-11ee-b9d1-0242ac120002','food and drink','2023-12-02 12:17',10000,1),
-       ('1f905152-92e3-11ee-b9d1-0242ac120002','gifts','2023-12-04 15:24',20,2),
-       ('1f9052ba-92e3-11ee-b9d1-0242ac120002','online shopping','2023-11-30 17:20',25000,1);
+insert into transaction (id, label, type, amount, account)
+values ('98ac0d10-92e3-11ee-b9d1-0242ac120002','food and drink','Debit',10000,'98ac0770-92e3-11ee-b9d1-0242ac120002'),
+       ('98ac0ee6-92e3-11ee-b9d1-0242ac120002','gifts','Debit',20,'98ac0b4e-92e3-11ee-b9d1-0242ac120002'),
+       ('98ac10bc-92e3-11ee-b9d1-0242ac120002','online shopping','Debit',25000,'98ac0928-92e3-11ee-b9d1-0242ac120002');
