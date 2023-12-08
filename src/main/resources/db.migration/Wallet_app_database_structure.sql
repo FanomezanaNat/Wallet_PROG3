76f71bc2c11b86d23642b44246f0723c362d284f
Sun-2302 <hei.dera.932@gmail.com>
2023-12-08 06:42:16 +0300

    feat: add the tables transferHistory and currencyValue to the database

--- src/main/resources/db.migration/Wallet_app_database_structure.sql
+++ src/main/resources/db.migration/Wallet_app_database_structure.sql
@@ -1,6 +1,6 @@
 create table IF NOt EXISTS currency
 (
-    id   uuid primary key not null,
+    id   uuid primary key,
     name varchar(50)      not null,
     code varchar(25)      not null
 );
@@ -10,7 +10,7 @@ create table IF NOt EXISTS currency
 create table IF NOT EXISTS account
 (
 
-    id         uuid primary key not null,
+    id         uuid primary key,
     name       varchar(150),
     balance    double precision,
     updateDate timestamp default current_timestamp,
@@ -21,7 +21,7 @@ create table IF NOT EXISTS account
 
 create table IF NOT EXISTS transaction
 (
-    id              uuid primary key                    not null,
+    id              uuid primary key,
     label           varchar(100)                        not null,
     type            varchar(150)                        not null,
     transactionDate timestamp default current_timestamp not null,
@@ -29,4 +29,19 @@ create table IF NOT EXISTS transaction
     Account         uuid references account (id)
 );
 
+create table IF NOT EXISTS transferHistory
+(
+    id uuid primary key,
+    debitTransactionId uuid references transaction(id) not null ,
+    creditTransactionId uuid references transaction(id) not null ,
+    transferDate timestamp default current_timestamp not null
+);
 
+create table IF NOT EXISTS currencyValue
+(
+    id uuid primary key ,
+    sourceCurrencyId uuid references currency(id) not null ,
+    destinationCurrencyId uuid references currency(id) not null ,
+    amount double precision not null ,
+    dateEffect timestamp not null
+);
