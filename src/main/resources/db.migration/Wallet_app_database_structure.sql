5de86a3bad79cc8625feb1bf0ab33df722469deb
FanomezanaNat <hei.fanomezana.2@gmail.com>
2023-12-13 11:29:27 +0300

    chore: reformat code

--- src/main/resources/db.migration/Wallet_app_database_structure.sql
+++ src/main/resources/db.migration/Wallet_app_database_structure.sql
@@ -1,8 +1,8 @@
 create table IF NOt EXISTS currency
 (
     id   uuid primary key,
-    name varchar(50)      not null,
-    code varchar(25)      not null
+    name varchar(50) not null,
+    code varchar(25) not null
 );
 
 
@@ -14,7 +14,7 @@ create table IF NOT EXISTS account
     name       varchar(150),
     balance    double precision,
     updateDate timestamp default current_timestamp,
-    type       varchar(150)     not null,
+    type       varchar(150) not null,
     Currency   uuid references currency (id)
 );
 
@@ -25,23 +25,23 @@ create table IF NOT EXISTS transaction
     label           varchar(100)                        not null,
     type            varchar(150)                        not null,
     transactionDate timestamp default current_timestamp not null,
-    amount          double precision                             not null,
+    amount          double precision                    not null,
     Account         uuid references account (id)
 );
 
 create table IF NOT EXISTS transferHistory
 (
-    id uuid primary key,
-    debitTransactionId uuid references transaction(id) not null ,
-    creditTransactionId uuid references transaction(id) not null ,
-    transferDate timestamp default current_timestamp not null
+    id                  uuid primary key,
+    debitTransactionId  uuid references transaction (id)    not null,
+    creditTransactionId uuid references transaction (id)    not null,
+    transferDate        timestamp default current_timestamp not null
 );
 
 create table IF NOT EXISTS currencyValue
 (
-    id uuid primary key ,
-    sourceCurrencyId uuid references currency(id) not null ,
-    destinationCurrencyId uuid references currency(id) not null ,
-    amount double precision not null ,
-    dateEffect timestamp not null
+    id                    uuid primary key,
+    sourceCurrencyId      uuid references currency (id) not null,
+    destinationCurrencyId uuid references currency (id) not null,
+    amount                double precision              not null,
+    dateEffect            timestamp                     not null
 );
