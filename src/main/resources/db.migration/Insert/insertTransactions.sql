0dcc5db11fd340187c7f2b6de15dd742938dbb2c
Sun-2302 <hei.dera.932@gmail.com>
2023-12-14 13:32:52 +0300

    feat: update insertTransactions

--- src/main/resources/db.migration/Insert/insertTransactions.sql
+++ src/main/resources/db.migration/Insert/insertTransactions.sql
@@ -1,4 +1,4 @@
-insert into transaction (id, label, type, amount, account)
-values ('98ac0d10-92e3-11ee-b9d1-0242ac120002','food and drink','Debit',10000,'98ac0770-92e3-11ee-b9d1-0242ac120002'),
-       ('98ac0ee6-92e3-11ee-b9d1-0242ac120002','gifts','Debit',20,'98ac0b4e-92e3-11ee-b9d1-0242ac120002'),
-       ('98ac10bc-92e3-11ee-b9d1-0242ac120002','online shopping','Debit',25000,'98ac0928-92e3-11ee-b9d1-0242ac120002');
+insert into transaction (id, amount, account, category)
+values ('98ac0d10-92e3-11ee-b9d1-0242ac120002', 10000,'98ac0770-92e3-11ee-b9d1-0242ac120002','98ac1b3e-92e3-11ee-b9d1-0242ac120002'),
+       ('98ac0ee6-92e3-11ee-b9d1-0242ac120002', 20,'98ac0b4e-92e3-11ee-b9d1-0242ac120002', '98ac1b3e-92e3-11ee-b9d1-0242ac120007'),
+       ('98ac10bc-92e3-11ee-b9d1-0242ac120002', 25000,'98ac0928-92e3-11ee-b9d1-0242ac120002', '98ac1b3e-92e3-11ee-b9d1-0242ac120003');
