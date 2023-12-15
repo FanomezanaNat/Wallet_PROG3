b66be43c7eaaeaaa631241fa921348ed0fec71a2
FanomezanaNat <hei.fanomezana.2@gmail.com>
2023-12-15 17:01:18 +0300

    feat: update MoneyFlowDAO.java

--- src/main/java/Wallet/DAO/MoneyFlowDAO.java
+++ src/main/java/Wallet/DAO/MoneyFlowDAO.java
@@ -1,12 +1,10 @@
 package Wallet.DAO;
 
 import Wallet.Entity.MoneyFlow;
-import Wallet.Entity.Transaction;
 import lombok.AllArgsConstructor;
 
 import java.sql.*;
-import java.util.ArrayList;
-import java.util.List;
+
 import java.util.UUID;
 
 @AllArgsConstructor
@@ -14,9 +12,8 @@ public class MoneyFlowDAO {
     private Connection connection;
 
     public MoneyFlow getMoneyFlowByDateRange(UUID idAccount, Date startDate, Date endDate){
-        List<Transaction> transactionList = new ArrayList<>();
-        String sql = "select sum(CASE WHEN t.name = 'credit' THEN transaction.amount ELSE 0 END) TotalEntrance, " +
-                "sum(CASE WHEN t.name = 'debit' THEN transaction.amount ELSE 0 END) TotalExit " +
+        String sql = "select sum(CASE WHEN category.type = 'credit' THEN transaction.amount ELSE 0 END) TotalEntrance, " +
+                "sum(CASE WHEN category.name = 'debit' THEN transaction.amount ELSE 0 END) TotalExit " +
                 "from transaction " +
                 "INNER JOIN category ON transaction.category = category.id " +
                 "INNER JOIN type t on category.type = t.id " +
