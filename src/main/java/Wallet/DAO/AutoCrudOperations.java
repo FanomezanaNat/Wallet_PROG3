146605c8ec20bd2d04b66667b20bfc7854796ba1
FanomezanaNat <hei.fanomezana.2@gmail.com>
2024-01-13 00:28:02 +0300

    feat: add findAll() in AutoCrudOperations

--- src/main/java/Wallet/DAO/AutoCrudOperations.java
+++ src/main/java/Wallet/DAO/AutoCrudOperations.java
@@ -2,14 +2,22 @@ package Wallet.DAO;
 
 import java.lang.reflect.*;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
-public class AutoCrudOperations<T>  implements CrudOperations<T> {
+public class AutoCrudOperations<T> implements CrudOperations<T> {
 
+    private static String FIND_ALL_QUERY = "select {COLUMNS} from {TABLES}";
     private Connection connection;
-    private static final String FIND_ALL_QUERY = "select {COLUMNS} from {TABLES}";
+
+
+    public AutoCrudOperations(Connection connection) {
+        this.connection = connection;
+    }
+
 
     private List<String> getAttributes(Class<T> clazz) throws IllegalAccessException {
         List<String> attributeNames = new ArrayList<>();
@@ -29,10 +37,37 @@ public class AutoCrudOperations<T>  implements CrudOperations<T> {
     @Override
     public List<T> findAll() {
 
-        List<Object> attributes;
-        String tableName;
+        List<T> resultList = new ArrayList<>();
+        Class<T> clazz = (Class<T>) ((ParameterizedType) getClass()
+                .getGenericSuperclass()).getActualTypeArguments()[0];
 
-        return null;
+        List<String> attributeNames;
+        try {
+            attributeNames = getAttributes(clazz);
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+            return resultList;
+        }
+        String tableName = getTableName(clazz);
+        String query = FIND_ALL_QUERY
+                .replace("{COLUMNS}", String.join(", ", attributeNames))
+                .replace("{TABLES}", tableName);
+        try (PreparedStatement statement = connection.prepareStatement(query)) {
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                T instance = clazz.getDeclaredConstructor().newInstance();
+                resultList.add(instance);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException | NoSuchMethodException | InstantiationException |
+                 InvocationTargetException e) {
+            throw new RuntimeException(e);
+        }
+
+        return resultList;
+
 
     }
 
