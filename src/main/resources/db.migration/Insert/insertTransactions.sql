59af7f03e3756d737f46ee875a549955f0aa6e2a
FanomezanaNat <hei.fanomezana.2@gmail.com>
2024-01-12 13:29:16 +0300

    fix: change excluded.amount to excluded.account

--- src/main/resources/db.migration/Insert/insertTransactions.sql
+++ src/main/resources/db.migration/Insert/insertTransactions.sql
@@ -6,5 +6,5 @@ values ('98ac0d10-92e3-11ee-b9d1-0242ac120002', 10000, '98ac0770-92e3-11ee-b9d1-
        ('98ac10bc-92e3-11ee-b9d1-0242ac120002', 25000, '98ac0928-92e3-11ee-b9d1-0242ac120002',
         '98ac1b3e-92e3-11ee-b9d1-0242ac120003')
 ON CONFLICT (id) DO UPDATE SET amount   = excluded.amount,
-                               account  = excluded.amount,
+                               account  = excluded.account,
                                category = excluded.category;
