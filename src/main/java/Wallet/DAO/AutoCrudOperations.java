1c74146d77658a4e50a071d1b15a67c8a52d7128
Sun-2302 <hei.dera.932@gmail.com>
2024-01-12 14:14:11 +0300

    feat: update AutoCrudOperation

--- src/main/java/Wallet/DAO/AutoCrudOperations.java
+++ src/main/java/Wallet/DAO/AutoCrudOperations.java
@@ -2,12 +2,27 @@ package Wallet.DAO;
 
 import java.lang.reflect.*;
 import java.sql.Connection;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
 public class AutoCrudOperations<T>  implements CrudOperations<T> {
+
     private Connection connection;
     private static String FIND_ALL_QUERY = "select {COLUMNS} from {TABLES}";
+
+    private List<String> getAttributes() throws IllegalAccessException {
+        List<String> attributeNames = new ArrayList<>();
+        Field[] fields = T.class.getDeclaredFields();
+
+        for (Field field : fields) {
+            field.setAccessible(true);
+            attributeNames.add(field.getName());
+        }
+
+        return attributeNames;
+    }
+
     @Override
     public List<T> findAll() {
         /*
