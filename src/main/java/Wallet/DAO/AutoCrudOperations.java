d39f5f08664450f185eb518f3958e09172cf2b98
Sun-2302 <hei.dera.932@gmail.com>
2024-01-12 13:54:56 +0300

    feat: add AutoCrudOperation

---
+++ src/main/java/Wallet/DAO/AutoCrudOperations.java
@@ -0,0 +1,41 @@
+package Wallet.DAO;
+
+import java.lang.reflect.*;
+import java.sql.Connection;
+import java.util.List;
+import java.util.Objects;
+
+public class AutoCrudOperations<T>  implements CrudOperations<T> {
+    private Connection connection;
+    private static String FIND_ALL_QUERY = "select {COLUMNS} from {TABLES}";
+    @Override
+    public List<T> findAll() {
+        /*
+        * Obtenir tous les attributs(utiles) de la classe T (id)
+        * Obtenir le nom de la table liée à T
+        * */
+
+        List<Object> attributes;
+        String tableName;
+
+        return connection.nativeSQL(FIND_ALL_QUERY
+                .replace("{COLUMNS}", {attributes})
+                .replace("{TABLES)",{tableName}));
+
+    }
+
+    @Override
+    public List<T> saveAll(List<T> toSave) {
+        return null;
+    }
+
+    @Override
+    public T save(T toSave) {
+        return null;
+    }
+
+    @Override
+    public T delete(T toDelete) {
+        return null;
+    }
+}
