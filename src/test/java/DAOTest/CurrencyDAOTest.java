aa6835949458fd6afbe82027635eac1077ba0ef5
Sun-2302 <hei.dera.932@gmail.com>
2024-01-29 21:37:35 +0300

    feat: add testSaveAll in CurrencyDAOTest

--- src/test/java/DAOTest/CurrencyDAOTest.java
+++ src/test/java/DAOTest/CurrencyDAOTest.java
@@ -4,6 +4,7 @@ import Wallet.DAO.AccountDAO;
 import Wallet.DAO.CurrencyDAO;
 import Wallet.DatabaseConfiguration.DatabaseConnection;
 import Wallet.Entity.Account;
+import Wallet.Entity.Currency;
 import org.junit.jupiter.api.Test;
 
 import java.sql.Connection;
@@ -30,4 +31,23 @@ public class CurrencyDAOTest {
         }
     }
 
+    @Test
+    void testSaveAll(){
+        DatabaseConnection connectionManager = new DatabaseConnection();
+        try (Connection connection = connectionManager.getConnection()){
+            CurrencyDAO currencyDAO = new CurrencyDAO(connection);
+            if (connection != null){
+                Currency currency1 = new Currency(UUID.fromString("d7157ff1-9030-4ca6-bbbe-64bed70e6368"),"Yuan","CNY");
+                Currency currency2 = new Currency(UUID.fromString("16f76bab-2299-4692-a3b9-53343837e183"),"Yen","JPY");
+                List<Currency> currenciesToSave = List.of(currency1, currency2);
+                assertEquals(2,currencyDAO.saveAll(currenciesToSave).size());
+                for (Currency currency : currenciesToSave){
+                    currencyDAO.delete(currency);
+                }
+            }
+        } catch (SQLException e) {
+            System.err.println("An error occurred while fetching accounts: " + e.getMessage());
+            System.out.println("Failed to retrieve accounts. Please try again later.");
+        }
+    }
 }
