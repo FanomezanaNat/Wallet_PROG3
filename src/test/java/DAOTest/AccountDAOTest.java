794517e3fc84f75a4eab51bd1eb0443ea8b4ea29
Sun-2302 <hei.dera.932@gmail.com>
2024-01-29 20:54:58 +0300

    feat: add testDelete in AccountDAOTest

--- src/test/java/DAOTest/AccountDAOTest.java
+++ src/test/java/DAOTest/AccountDAOTest.java
@@ -9,7 +9,6 @@ import static org.junit.jupiter.api.Assertions.*;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Timestamp;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -58,6 +57,23 @@ public class AccountDAOTest {
                 Account createdAccount = accountDAO.save(account);
                 assertNotNull(createdAccount);
                 accountDAO.delete(account);
+            }
+        } catch (SQLException e) {
+            System.err.println("An error occurred while fetching accounts: " + e.getMessage());
+            System.out.println("Failed to retrieve accounts. Please try again later.");
+        }
+    }
+
+    @Test
+    void testDelete(){
+        DatabaseConnection connectionManager = new DatabaseConnection();
+        try (Connection connection = connectionManager.getConnection()){
+            AccountDAO accountDAO = new AccountDAO(connection);
+            if (connection != null){
+                Account account = new Account(UUID.fromString("77386308-7908-4642-8b2a-7f2e54e9b2d3"),"",300000.0,new Timestamp(System.currentTimeMillis()),"Mobile Money",UUID.fromString("98abfe06-92e3-11ee-b9d1-0242ac120002"));
+                Account createdAccount = accountDAO.save(account);
+                Account deletedAccount = accountDAO.delete(account);
+                assertEquals(createdAccount, deletedAccount);
             }
         } catch (SQLException e) {
             System.err.println("An error occurred while fetching accounts: " + e.getMessage());
